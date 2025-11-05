58f412c22acc7b26315843a3515ce39377d8eb56
BryanManueke <manuekebryan06@gmail.com>
2025-11-05 20:38:03 +0800

    Bryan-Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-800 text-white py-4 text-center">
+      <p>© 2025 Berita Hari Ini | Sumber Informasi Terpercaya</p>
+    </footer>
+  );
+}
